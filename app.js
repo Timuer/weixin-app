5efc74aea12cbb5e9de05a9909ad4fcf89c8398f
Timuer <m15927177412@163.com>
2018-07-22 10:29:43 +0800

    complete auth

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const config = require('./config')
 const Koa = require('koa')
+const sha1 = require('sha1')
 const route = require('koa-route')
 const serve = require('koa-static')
 
 const app = new Koa()
 
-const main = ctx => {
-    ctx.response.body = "Hello You"
+const auth = ctx => {
+    const q = ctx.request.query
+    const token = config.wechat.token
+    const nonce = q.nonce
+    const timestamp = q.timestamp
+    const s = sha1([token, nonce, timestamp].sort().join(''))
+    const signature = q.signature
+    const ecostr = q.ecostr
+    if (s == signature) {
+        ctx.response.body = ecostr
+    } else {
+        ctx.response.body = 'Wrong'
+    }
 }
 
-app.use(main)
+app.use(auth)
 
-app.listen(3000)
+app.listen(80)
